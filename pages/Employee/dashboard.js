9039b6ebdfb3d66a91d1ca9462b5dfd6dbb13361
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-08-06 01:02:12 +0600

    nav bar , side bar

---
+++ pages/Employee/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../components/sidebar";
+import Navbar2 from "../components/navbar2";
+
+export default function Dashboard() {
+	return (
+		<div>
+			<Navbar2 />
+			<Sidebar />
+		</div>
+	);
+}
